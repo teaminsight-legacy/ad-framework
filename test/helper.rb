124e73009ba2d417f057c3ab4e7cf78de0621a27
jcredding <TempestTTU@gmail.com>
2011-08-18 14:48:48 -0500

    ignore the private folder

--- test/helper.rb
+++ test/helper.rb
@@ -10,6 +10,27 @@ require 'test/support/attributes'
 require 'test/support/attribute_types'
 require 'test/support/structural_class'
 
+
+
 AD::Framework.configure do |config|
+  config.ldap do |ldap|
+    
+  end
   config.treebase = "DC=example, DC=com"
 end
+
+=begin
+@module.configure do |config|
+  config.ldap do |ldap|
+    ldap.host = host
+    ldap.port = port
+    ldap.encryption = encryption
+    ldap.auth = auth
+  end
+  config.logger = logger
+  config.search_size_supported = search_size
+  config.mappings = mappings
+  config.run_commands = run_commands
+  config.treebase = treebase
+end
+=end
